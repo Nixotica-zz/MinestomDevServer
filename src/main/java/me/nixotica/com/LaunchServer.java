dcb4d964aeb6e7a58326c234aa55f167305ddf56
nickwalte <nickwalt@umich.edu>
2021-12-08 18:19:06 -0500

    add dev server to git

---
+++ src/main/java/me/nixotica/com/LaunchServer.java
@@ -0,0 +1,9 @@
+package me.nixotica.com;
+
+import net.minestom.server.Bootstrap;
+
+public class LaunchServer {
+    public static void main(String[] args) {
+        Bootstrap.bootstrap("me.nixotica.com.StartServer", args);
+    }
+}
